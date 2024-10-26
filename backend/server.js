9dbe5af46fc507ef427719862ebc013967071ee1
ALVIN-OBONYO <alvinnmichael@gmail.com>
2024-10-27 00:40:31 +0300

    projo

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+// const express = require('express');
+import express from 'express';
+
+const app = express();
+
+app.get("/", (req,res) => {
+  res.send("Server is ready");
+});
+
+app.listen(5000, () => {
+  console.log('Server started at http://localhost:5000');
+  
+});
+
+
+//password uOuH1MOYNbDWOJOj
